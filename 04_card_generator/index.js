ae15bc3536c0649c55073d92e986523707a9f6e3
Nick Handy <nichandy@gmail.com>
2021-02-28 17:53:34 -0800

    refactor filenames and remove unneeded files

--- 04_card_generator/index.js
+++
@@ -1,4 +0,0 @@
-const cardUtils = require('./utils/cardUtils');
-const createCardData = require('./utils/createCardData');
-
-cardUtils.generateDecks();
--- 05_remark_playground/index.js
+++
@@ -1,8 +0,0 @@
-const fse = require('fs-extra');
-const markdowngfm = require('./markdowngfm');
-
-let filenames = fse.readdirSync('./flashcards');
-filenames.map((filename) => {
-  let doc = fse.readFileSync(`./flashcards/${filename}`);
-  markdowngfm(doc);
-});
